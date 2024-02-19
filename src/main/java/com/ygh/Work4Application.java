1f3b4878f3ec1d981f66c1344a4e8b3fc672ae3a
“1293978818” <1293978818>
2024-02-19 18:48:59 +0800

    add:work4 start here

---
+++ src/main/java/com/ygh/Work4Application.java
@@ -0,0 +1,16 @@
+package com.ygh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ygh
+ */
+@SpringBootApplication
+public class Work4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Work4Application.class, args);
+	}
+
+}
